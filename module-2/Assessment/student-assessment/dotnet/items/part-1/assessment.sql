e1bfb63a8908fcc7b28a1ea69eb42f64fa85f2cd
Adam Tulloss <artulloss@gmail.com>
2020-11-13 10:13:14 -0500

    Assessment

--- module-2/Assessment/student-assessment/dotnet/items/part-1/assessment.sql
+++ module-2/Assessment/student-assessment/dotnet/items/part-1/assessment.sql
@@ -3,10 +3,24 @@ GO
 
 -- Select all columns from users where the user's role is admin
 
+SELECT * FROM users WHERE role = 'admin';
+
 -- Select name and description from items that were created after Sept. 20, 2019 and the description isn't null
+
+SELECT name, description FROM items WHERE description IS NOT NULL AND created > CONVERT(datetime, '2019-09-20');
 
 -- Select first_name and last_name from users and order by when they were created, latest first
 
+SELECT first_name, last_name FROM users ORDER BY created DESC;
+
 -- Select finished and a count all the items that are finished/not finished
 
+SELECT finished, COUNT(*) AS count FROM items GROUP BY finished;
+
 -- Select a user's first_name and last_name and the item's name for every finished item
+
+SELECT first_name, last_name, name FROM users u
+INNER JOIN items i ON i.user_id = u.id;
+-- This will work as well, just more specific, unnecessary because no overlapping column names
+SELECT u.first_name, u.last_name, i.name FROM users u
+INNER JOIN items i ON i.user_id = u.id;
--- module-2/Assessment/student-assessment/dotnet/items/part-2/Items/Items/UserSqlDao.cs
+++ module-2/Assessment/student-assessment/dotnet/items/part-2/Items/Items/UserSqlDao.cs
@@ -16,13 +16,68 @@ namespace Items
         public IList<User> GetAllUsers()
         {
             // Implement this method to pull in all users from database
+            IList<User> users = new List<User>();
 
-            return null;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM users", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (!reader.HasRows) return users;
+                    while (reader.Read())
+                    {
+                        users.Add(ReaderToUser(reader));
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return users;
         }
 
         public void Save(User newUser)
         {
-            // Implement this method to save user to database
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand insertCmd = new SqlCommand(@"INSERT INTO users(first_name, last_name, email, role, created)
+                            VALUES (@first_name, @last_name, @email, @role, @created)", conn);
+                    // Safely add values into sql
+                    insertCmd.Parameters.AddWithValue("@first_name", newUser.FirstName);
+                    insertCmd.Parameters.AddWithValue("@last_name", newUser.LastName);
+                    insertCmd.Parameters.AddWithValue("@email", newUser.Email);
+                    insertCmd.Parameters.AddWithValue("@role", newUser.Role);
+                    insertCmd.Parameters.AddWithValue("@created", newUser.Created);
+                    insertCmd.ExecuteNonQuery();
+                    SqlCommand selectCmd = new SqlCommand("SELECT TOP 1 @@IDENTITY FROM users", conn);
+                    newUser.Id = Convert.ToInt64(selectCmd.ExecuteScalar());
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        private static User ReaderToUser(SqlDataReader reader)
+        {
+            User user = new User
+            {
+                Id = (int) reader["id"], // Casting or using Convert.To<Type> works here
+                FirstName = Convert.ToString(reader["first_name"]),
+                LastName = Convert.ToString(reader["last_name"]),
+                Email = Convert.ToString(reader["email"]),
+                Role = Convert.ToString(reader["role"]),
+                Created = Convert.ToDateTime(reader["created"])
+            };
+            return user;
         }
     }
 }
