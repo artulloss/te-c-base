8580ba85668955d3791e24e2ad835a5a175f9a69
Adam Tulloss <artulloss@gmail.com>
2020-11-19 16:08:39 -0500

    Lecture 11

--- module-3/23_Event_Handling/student-lecture/play-around-before-lecture/app.js
+++ module-3/23_Event_Handling/student-lecture/play-around-before-lecture/app.js
@@ -1,16 +1,28 @@
-let myButton = document.getElementById('the_button');
-myButton.addEventListener('click',showButtonAlert) // no parentheses
+let myButton = document.getElementById("the_button");
+myButton.addEventListener("click", showButtonAlert); // no parentheses
 
-function showButtonAlert(event){
-    alert('You clicked button' + event.target);
+function showButtonAlert(event) {
+  alert("You clicked button" + event.target);
 }
 
-let myDiv  = document.getElementById('the-div');
-myDiv.addEventListener('mouseover',(event)=>{
-   event.target.classList.remove('blue');
-   event.target.classList.add('red');
+let myDiv = document.getElementById("the-div");
+myDiv.addEventListener("mouseover", (event) => {
+  event.target.classList.remove("blue");
+  event.target.classList.add("red");
 });
-myDiv.addEventListener('mouseout',(event)=>{
-    event.target.classList.remove('red');
-    event.target.classList.add('blue');
- });
+
+myDiv.addEventListener("mouseout", (event) => {
+  event.target.classList.remove("red");
+  event.target.classList.add("blue");
+});
+
+const groceryButton = document.getElementById("grocery-button");
+0;
+
+groceryButton.addEventListener("click", function fff(ee) {
+  const groceryList = document.getElementById("the-ul");
+  const groceryItem = document.createElement("li");
+  groceryItem.innerText = "Coffie";
+  groceryList.appendChild(groceryItem);
+  //groceryButton.removeEventListener("click", fff); // This works because you can name a normal function
+});
--- module-3/23_Event_Handling/student-lecture/product-reviews/js/app.js
+++ module-3/23_Event_Handling/student-lecture/product-reviews/js/app.js
@@ -1,13 +1,14 @@
-const name = 'Cigar Parties for Dummies';
-let description = 'Host and plan the perfect cigar party for all of your squirrelly friends.';
+const name = "Cigar Parties for Dummies";
+let description =
+  "Host and plan the perfect cigar party for all of your squirrelly friends.";
 let reviews = [
   {
-    reviewer: 'Malcolm Gladwell',
-    title: 'What a book!',
+    reviewer: "Malcolm Gladwell",
+    title: "What a book!",
     review:
       "It certainly is a book. I mean, I can see that. Pages kept together with glue (I hope that's glue) and there's writing on it, in some language.",
-    rating: 3
-  }
+    rating: 3,
+  },
 ];
 
 /**
@@ -16,27 +17,27 @@ let reviews = [
  * once you have the element you can add the product name to the span.
  */
 function setPageTitle() {
-  const pageTitle = document.getElementById('page-title');
-  pageTitle.querySelector('.name').innerHTML = name;
+  const pageTitle = document.getElementById("page-title");
+  pageTitle.querySelector(".name").innerHTML = name;
 }
 
 /**
  * Add our product description to the page.
  */
 function setPageDescription() {
-  document.querySelector('.description').innerHTML = description;
+  document.querySelector(".description").innerHTML = description;
 }
 
 /**
  * I will display all of the reviews in the reviews array
  */
 function displayReviews() {
-  if ('content' in document.createElement('template')) {
+  if ("content" in document.createElement("template")) {
     reviews.forEach((review) => {
       displayReview(review);
     });
   } else {
-    console.error('Your browser does not support templates');
+    console.error("Your browser does not support templates");
   }
 }
 
@@ -45,27 +46,56 @@ function displayReviews() {
  * @param {Object} review The review to display
  */
 function displayReview(review) {
-  const main = document.getElementById('main');
-  const tmpl = document.getElementById('review-template').content.cloneNode(true);
-  tmpl.querySelector('h4').innerHTML = review.reviewer;
-  tmpl.querySelector('h3').innerHTML = review.title;
-  tmpl.querySelector('p').innerHTML = review.review;
+  const main = document.getElementById("main");
+  const tmpl = document
+    .getElementById("review-template")
+    .content.cloneNode(true);
+  tmpl.querySelector("h4").innerHTML = review.reviewer;
+  tmpl.querySelector("h3").innerHTML = review.title;
+  tmpl.querySelector("p").innerHTML = review.review;
   // there will always be 1 star because it is a part of the template
   for (let i = 1; i < review.rating; ++i) {
-    const img = tmpl.querySelector('img').cloneNode();
-    tmpl.querySelector('.rating').appendChild(img);
+    const img = tmpl.querySelector("img").cloneNode();
+    tmpl.querySelector(".rating").appendChild(img);
   }
   main.appendChild(tmpl);
 }
 
 // LECTURE STARTS HERE ---------------------------------------------------------------
 
-// set the product reviews page title
-setPageTitle();
-// set the product reviews page description
-setPageDescription();
-// display all of the product reviews on our page
-displayReviews();
+document.addEventListener("DOMContentLoaded", () => {
+  setPageTitle();
+  setPageDescription();
+  displayReviews();
+  const desc = document.querySelector(".description");
+
+  desc.addEventListener("click", (event) => {
+    toggleDescriptionEdit(event.target);
+  });
+
+  const inputDesc = document.getElementById("inputDesc");
+  inputDesc.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      exitDescriptionEdit(event.target, true);
+    }
+    if (event.key === "Escape") {
+      exitDescriptionEdit(event.target, false);
+    }
+  });
+  inputDesc.addEventListener("mouseleave", (event) => {
+    exitDescriptionEdit(event.target, false);
+  });
+
+  const btnToggleForm = document.getElementById("btnToggleForm");
+  btnToggleForm.addEventListener("click", showHideForm);
+
+  const btnSaveReview = document.getElementById("btnSaveReview");
+  btnSaveReview.addEventListener("click", (event) => {
+    event.preventDefault(); // Lets not refresh the page
+    saveReview();
+    showHideForm();
+  });
+});
 
 /**
  * Take an event on the description and swap out the description for a text box.
@@ -75,8 +105,8 @@ displayReviews();
 function toggleDescriptionEdit(desc) {
   const textBox = desc.nextElementSibling;
   textBox.value = description;
-  textBox.classList.remove('d-none');
-  desc.classList.add('d-none');
+  textBox.classList.remove("d-none");
+  desc.classList.add("d-none");
   textBox.focus();
 }
 
@@ -91,26 +121,27 @@ function exitDescriptionEdit(textBox, save) {
   let desc = textBox.previousElementSibling;
   if (save) {
     desc.innerText = textBox.value;
+    description = textBox.value;
   }
-  textBox.classList.add('d-none');
-  desc.classList.remove('d-none');
+  textBox.classList.add("d-none");
+  desc.classList.remove("d-none");
 }
 
 /**
  * I will show / hide the add review form
  */
 function showHideForm() {
-  const form = document.querySelector('form');
-  const btn = document.getElementById('btnToggleForm');
+  const form = document.querySelector("form");
+  const btn = document.getElementById("btnToggleForm");
 
-  if (form.classList.contains('d-none')) {
-    form.classList.remove('d-none');
-    btn.innerText = 'Hide Form';
-    document.getElementById('name').focus();
+  if (form.classList.contains("d-none")) {
+    form.classList.remove("d-none");
+    btn.innerText = "Hide Form";
+    document.getElementById("name").focus();
   } else {
     resetFormValues();
-    form.classList.add('d-none');
-    btn.innerText = 'Add Review';
+    form.classList.add("d-none");
+    btn.innerText = "Add Review";
   }
 }
 
@@ -118,16 +149,25 @@ function showHideForm() {
  * I will reset all of the values in the form.
  */
 function resetFormValues() {
-  const form = document.querySelector('form');
-  const inputs = form.querySelectorAll('input');
+  const form = document.querySelector("form");
+  const inputs = form.querySelectorAll("input");
   inputs.forEach((input) => {
-    input.value = '';
+    input.value = "";
   });
-  document.getElementById('rating').value = 1;
-  document.getElementById('review').value = '';
+  document.getElementById("rating").value = 1;
+  document.getElementById("review").value = "";
 }
 
 /**
  * I will save the review that was added using the add review from
  */
-function saveReview() {}
+function saveReview() {
+  const newReview = {
+    reviewer: document.getElementById("name").value,
+    title: document.getElementById("title").value,
+    review: document.getElementById("review").value,
+    rating: document.getElementById("rating").value,
+  };
+  reviews.push(newReview);
+  displayReview(newReview);
+}
