3818f50608e77d3eec4021170553468506337cc8
Adam Tulloss <artulloss@gmail.com>
2020-11-23 15:11:37 -0500

    Homework 11-23-30

--- module-3/30_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
+++ module-3/30_Introduction_to_Vue_and_Data_Binding/student-exercise/src/components/UserList.vue
@@ -1,22 +1,30 @@
 <template>
   <table id="tblUsers">
     <thead>
-    <tr>
+      <tr>
         <th>First Name</th>
         <th>Last Name</th>
         <th>Username</th>
         <th>Email Address</th>
         <th>Status</th>
-    </tr>
+      </tr>
     </thead>
     <tbody>
       <tr>
-        <td><input type="text" id="firstNameFilter"/></td>
-        <td><input type="text" id="lastNameFilter"/></td>
-        <td><input type="text" id="usernameFilter"/></td>
-        <td><input type="text" id="emailFilter"/></td>
         <td>
-          <select id="statusFilter">
+          <input type="text" id="firstNameFilter" v-model="filter.firstName" />
+        </td>
+        <td>
+          <input type="text" id="lastNameFilter" v-model="filter.lastName" />
+        </td>
+        <td>
+          <input type="text" id="usernameFilter" v-model="filter.username" />
+        </td>
+        <td>
+          <input type="text" id="emailFilter" v-model="filter.emailAddress" />
+        </td>
+        <td>
+          <select id="statusFilter" v-model="filter.status">
             <option value="">Show All</option>
             <option value="Active">Active</option>
             <option value="Disabled">Disabled</option>
@@ -24,35 +32,107 @@
         </td>
       </tr>
       <!-- user listing goes here -->
+      <tr
+        v-for="u in filteredList"
+        :key="u.username"
+        v-bind:class="{ disabled: u.status === 'Disabled' }"
+      >
+        <td>{{ u.firstName }}</td>
+        <td>{{ u.lastName }}</td>
+        <td>{{ u.username }}</td>
+        <td>{{ u.emailAddress }}</td>
+        <td>{{ u.status }}</td>
+      </tr>
     </tbody>
   </table>
 </template>
 
 <script>
 export default {
-  name: 'user-list',
+  name: "user-list",
   data() {
     return {
       users: [
-        { firstName: 'John', lastName: 'Smith', username: 'jsmith', emailAddress: 'jsmith@gmail.com', status: 'Active' },
-        { firstName: 'Anna', lastName: 'Bell', username: 'abell', emailAddress: 'abell@yahoo.com', status: 'Active' },
-        { firstName: 'George', lastName: 'Best', username: 'gbest', emailAddress: 'gbest@gmail.com', status: 'Disabled' },
-        { firstName: 'Ben', lastName: 'Carter', username: 'bcarter', emailAddress: 'bcarter@gmail.com', status: 'Active' },
-        { firstName: 'Katie', lastName: 'Jackson', username: 'kjackson', emailAddress: 'kjackson@yahoo.com', status: 'Active' },
-        { firstName: 'Mark', lastName: 'Smith', username: 'msmith', emailAddress: 'msmith@foo.com', status: 'Disabled' }
-      ]
-    }
-  }
-}
+        {
+          firstName: "John",
+          lastName: "Smith",
+          username: "jsmith",
+          emailAddress: "jsmith@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Anna",
+          lastName: "Bell",
+          username: "abell",
+          emailAddress: "abell@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "George",
+          lastName: "Best",
+          username: "gbest",
+          emailAddress: "gbest@gmail.com",
+          status: "Disabled",
+        },
+        {
+          firstName: "Ben",
+          lastName: "Carter",
+          username: "bcarter",
+          emailAddress: "bcarter@gmail.com",
+          status: "Active",
+        },
+        {
+          firstName: "Katie",
+          lastName: "Jackson",
+          username: "kjackson",
+          emailAddress: "kjackson@yahoo.com",
+          status: "Active",
+        },
+        {
+          firstName: "Mark",
+          lastName: "Smith",
+          username: "msmith",
+          emailAddress: "msmith@foo.com",
+          status: "Disabled",
+        },
+      ],
+      filter: {
+        firstName: "",
+        lastName: "",
+        username: "",
+        emailAddress: "",
+        status: "",
+      },
+    };
+  },
+  computed: {
+    // This should be called filteredList - lists aren't a thing in js and it's not descriptive
+    filteredList() {
+      const compare = (string, compare) =>
+        compare === "" || string.toLowerCase().includes(compare.toLowerCase());
+      const filter = this.filter;
+      return this.users.filter((u) => {
+        return (
+          compare(u.firstName, filter.firstName) &&
+          compare(u.lastName, filter.lastName) &&
+          compare(u.username, filter.username) &&
+          compare(u.emailAddress, filter.emailAddress) &&
+          compare(u.status, filter.status)
+        );
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
 table {
   margin-top: 20px;
-  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 }
 th {
-  text-transform: uppercase
+  text-transform: uppercase;
 }
 td {
   padding: 10px;
@@ -60,7 +140,8 @@ td {
 tr.disabled {
   color: red;
 }
-input, select {
+input,
+select {
   font-size: 16px;
 }
 </style>
