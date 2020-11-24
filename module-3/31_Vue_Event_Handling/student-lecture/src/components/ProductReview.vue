25ac9a40f94a48e8bd68424ba5479d449fc27336
Adam Tulloss <artulloss@gmail.com>
2020-11-24 12:32:27 -0500

    Lecture

--- module-3/31_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
+++ module-3/31_Vue_Event_Handling/student-lecture/src/components/ProductReview.vue
@@ -5,41 +5,79 @@
     <p class="description">{{ description }}</p>
 
     <div class="well-display">
-      <div class="well">
+      <div class="well" v-on:click="filterRating = 0">
         <span class="amount">{{ averageRating }}</span>
         Average Rating
       </div>
 
-      <div class="well">
-        <span class="amount">{{ numberOfOneStarReviews }}</span>
-        1 Star Review{{ numberOfOneStarReviews === 1 ? '' : 's' }}
+      <div class="well" v-on:click="filterRating = 1">
+        <span class="amount">{{ numberOfReviews(1) }}</span>
+        1 Star Review{{ numberOfReviews(1) === 1 ? "" : "s" }}
       </div>
 
-      <div class="well">
-        <span class="amount">{{ numberOfTwoStarReviews }}</span>
-        2 Star Review{{ numberOfTwoStarReviews === 1 ? '' : 's' }}
+      <div class="well" v-on:click="filterRating = 2">
+        <span class="amount">{{ numberOfReviews(2) }}</span>
+        2 Star Review{{ numberOfReviews(2) === 1 ? "" : "s" }}
       </div>
 
-      <div class="well">
-        <span class="amount">{{ numberOfThreeStarReviews }}</span>
-        3 Star Review{{ numberOfThreeStarReviews === 1 ? '' : 's' }}
+      <div class="well" v-on:click="filterRating = 3">
+        <span class="amount">{{ numberOfReviews(3) }}</span>
+        3 Star Review{{ numberOfReviews(3) === 1 ? "" : "s" }}
       </div>
 
-      <div class="well">
-        <span class="amount">{{ numberOfFourStarReviews }}</span>
-        4 Star Review{{ numberOfFourStarReviews === 1 ? '' : 's' }}
+      <div class="well" v-on:click="filterRating = 4">
+        <span class="amount">{{ numberOfReviews(4) }}</span>
+        4 Star Review{{ numberOfReviews(4) === 1 ? "" : "s" }}
       </div>
 
-      <div class="well">
-        <span class="amount">{{ numberOfFiveStarReviews }}</span>
-        5 Star Review{{ numberOfFiveStarReviews === 1 ? '' : 's' }}
+      <div class="well" v-on:click="filterRating = 5">
+        <span class="amount">{{ numberOfReviews(5) }}</span>
+        5 Star Review{{ numberOfReviews(5) === 1 ? "" : "s" }}
       </div>
     </div>
+    <a
+      href="Show form"
+      v-on:click.prevent="showForm = true"
+      v-if="showForm === false"
+      >Show Form</a
+    >
+    <form v-on:submit.prevent="submitNewReview" v-if="showForm === true">
+      <div class="form-element">
+        <label for="reviewer">Name</label>
+        <input id="reviewer" type="text" v-model="newReview.reviewer" />
+      </div>
+      <div class="form-element">
+        <label for="title">Title</label>
+        <input id="title" type="text" v-model="newReview.title" />
+      </div>
+      <div class="form-element">
+        <label for="review">Review</label>
+        <textarea id="review" cols="300" rows="60" v-model="newReview.review" />
+      </div>
+      <div class="form-element">
+        <label for="rating">Number of Stars</label>
+        <input
+          id="rating"
+          type="number"
+          min="1"
+          max="5"
+          v-model.number="newReview.rating"
+        />
+      </div>
+      <div class="form-element">
+        <label for="favorited">Favorite?</label>
+        <input id="favorited" type="checkbox" v-model="newReview.favorited" />
+      </div>
+      <div class="form-element">
+        <input id="submit" type="submit" value="Save" />
+        <input type="button" value="Cancel" v-on:click="resetForm" />
+      </div>
+    </form>
 
     <div
       class="review"
       v-bind:class="{ favorited: review.favorited }"
-      v-for="review in reviews"
+      v-for="review in filteredReviews"
       v-bind:key="review.id"
     >
       <h4>{{ review.reviewer }}</h4>
@@ -73,6 +111,8 @@ export default {
       description:
         "Host and plan the perfect cigar party for all of your squirrelly friends.",
       newReview: {},
+      showForm: false,
+      filterRating: 0,
       reviews: [
         {
           reviewer: "Malcolm Gladwell",
@@ -80,7 +120,7 @@ export default {
           review:
             "It certainly is a book. I mean, I can see that. Pages kept together with glue and there's writing on it, in some language.",
           rating: 3,
-          favorited: false
+          favorited: false,
         },
         {
           reviewer: "Tim Ferriss",
@@ -88,7 +128,7 @@ export default {
           review:
             "It should have been called the four hour cigar party. That's amazing. I have a new idea for muse because of this.",
           rating: 4,
-          favorited: false
+          favorited: false,
         },
         {
           reviewer: "Ramit Sethi",
@@ -96,7 +136,7 @@ export default {
           review:
             "When I sell my courses, I'm always telling people that if a book costs less than $20, they should just buy it. If they only learn one thing from it, it was worth it. Wish I learned something from this book.",
           rating: 1,
-          favorited: false
+          favorited: false,
         },
         {
           reviewer: "Gary Vaynerchuk",
@@ -104,44 +144,44 @@ export default {
           review:
             "There are a lot of good, solid tips in this book. I don't want to ruin it, but prelighting all the cigars is worth the price of admission alone.",
           rating: 3,
-          favorited: false
-        }
-      ]
+          favorited: false,
+        },
+      ],
     };
   },
   computed: {
     averageRating() {
+      const length = this.reviews.length;
       let sum = this.reviews.reduce((currentSum, review) => {
         return currentSum + review.rating;
       }, 0);
-      return (sum / this.reviews.length).toFixed(2);
+      return (sum / length).toFixed(2);
     },
-    numberOfOneStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 1);
-      }, 0);
+    filteredReviews() {
+      return this.reviews.filter(
+        (review) =>
+          review.rating === this.filterRating || this.filterRating === 0
+      );
     },
-    numberOfTwoStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 2);
-      }, 0);
+  },
+  methods: {
+    submitNewReview(event) {
+      event.preventDefault();
+      console.log("called");
+      this.reviews.unshift(this.newReview);
+      this.resetForm();
     },
-    numberOfThreeStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 3);
-      }, 0);
+    resetForm() {
+      this.newReview = {};
+      this.showForm = false;
     },
-    numberOfFourStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 4);
-      }, 0);
+    numberOfReviews(rating = 0) {
+      return this.reviews.reduce(
+        (acc, review) => acc + Number(review.rating === rating || rating === 0),
+        0
+      );
     },
-    numberOfFiveStarReviews() {
-      return this.reviews.reduce((currentCount, review) => {
-        return currentCount + (review.rating === 5);
-      }, 0);
-    }
-  }
+  },
 };
 </script>
 
@@ -210,7 +250,8 @@ div.form-element {
 div.form-element > label {
   display: block;
 }
-div.form-element > input, div.form-element > select {
+div.form-element > input,
+div.form-element > select {
   height: 30px;
   width: 300px;
 }
@@ -218,12 +259,14 @@ div.form-element > textarea {
   height: 60px;
   width: 300px;
 }
-form > input[type=button] {
+form > input[type="button"] {
   width: 100px;
 }
-form > input[type=submit] {
+form > input[type="submit"] {
   width: 100px;
   margin-right: 10px;
 }
+.hidden {
+  display: none;
+}
 </style>
-
